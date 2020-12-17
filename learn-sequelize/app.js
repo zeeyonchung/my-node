cc7304fc5eb96c9232bbc85a62c9e8e1b9a008ca
Zeeyon <zeeyon.c@gmail.com>
2020-12-17 22:15:07 +0900

    front 정적 파일 추가

--- learn-sequelize/app.js
+++ learn-sequelize/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var commentsRouter = require('./routes/comments');
 var { sequelize } = require('./models/index2');
 
 var app = express();
@@ -13,7 +14,7 @@ sequelize.sync();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
